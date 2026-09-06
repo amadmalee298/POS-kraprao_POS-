bc59cf455c71d9d4e18387217338dbbee66b301f
amadmalee298 <amadmalee298@gmail.com>
2026-09-06 15:18:49 +0700

    refactor(pos): import useCallback in POSContext
    Import useCallback to optimize memoized functions within the POS context provider.

--- src/context/POSContext.tsx
+++ src/context/POSContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import React, { createContext, useContext, useState, useEffect, useCallback, ReactNode } from 'react';
 import { calcRecipeItemCostAndDeduction } from '../utils/recipeUtils';
 import { syncAndHealCategories, syncAndHealIngredientCategories } from '../utils/categoryUtils';
 import {
